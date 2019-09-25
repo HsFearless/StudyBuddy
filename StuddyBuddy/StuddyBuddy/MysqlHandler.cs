3e2bb0abaf355d51e8ef1f80e2a0de75efdaf5af
user0481 <apelsinas.m@gmail.com>
2019-09-25 19:38:02 +0300

    step000

---
+++ StuddyBuddy/StuddyBuddy/MysqlHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace StuddyBuddy
+{
+    class MysqlHandler
+    {
+        private string host;
+        private string username;
+        private string password;
+        private string database;
+        private int port;
+        //private Data.MySqlClient a;
+
+        public MysqlHandler(string host, string username, string password, string database, int port)
+        {
+            this.host = host;
+            this.username = username;
+            this.password = password;
+            this.database = database;
+            this.port = port;
+        }
+        
+        public MysqlHandler()
+        {
+            host = "remotemysql.com";
+            username = "cDKryxhEGc";
+            password = "rw0faFJV0H";
+            database = "cDKryxhEGc";
+            port = 3306;
+        }
+
+
+
+    }
+}
--- StuddyBuddy/StuddyBuddy/RegisterForm.cs
+++ StuddyBuddy/StuddyBuddy/RegisterForm.cs
@@ -16,5 +16,6 @@ namespace StuddyBuddy
         {
             InitializeComponent();
         }
+
     }
 }
