fd4e004e4bb5e9eec92644430fa06cad744b4c22
BMartynas <55427679+BMartynas@users.noreply.github.com>
2019-09-26 16:20:27 +0300

    Add files via upload

---
+++ studyBuddy/studyBuddy/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studyBuddy
+{
+    public partial class createProblemForm : Form
+    {
+
+        forumForm forumForm;
+        public createProblemForm(forumForm forumForm)
+        {
+            this.forumForm = forumForm;
+            InitializeComponent();
+        }
+
+        private void CreateProblemForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Co_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            problemsNameTextBox.ResetText();
+            problemsDescriptionTextBox.ResetText();
+            forumForm.Show();
+        }
+    }
+}
