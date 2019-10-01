4e1de950f6bd2913508a17e1d1c9e53e918f454e
user0481 <apelsinas.m@gmail.com>
2019-10-01 18:05:19 +0300

    organised forms and their content. could not launch app due to missing RoundButton.cs so I downloaded it from earlier versions.
    Pay attention to added additional Reference - MySQL Data
    You might need to install it if you get compile errors.
    Link: https://dev.mysql.com/downloads/connector/net/1.0.html

---
+++ studyBuddy/studyBuddy/dataNeeds/dataFetcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyBuddy.dataNeeds
+{
+    class DataFetcher
+    {
+        MysqlHandler source = new MysqlHandler();
+
+        public string getSalt(string username)
+        {
+            return "aaaaa";
+        }
+
+        public int getKarma(string username)
+        {
+            return 0;
+        }
+    }
+}
