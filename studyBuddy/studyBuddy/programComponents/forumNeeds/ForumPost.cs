b80203f3df007f29960f063ce8317a3f8acc587f
user0481 <egisxxegis@gmail.com>
2019-10-14 17:14:15 +0300

    ForumPost. step0

--- studyBuddy/studyBuddy/programComponents/forumNeeds/ForumPost.cs
+++ studyBuddy/studyBuddy/programComponents/forumNeeds/ForumPost.cs
@@ -8,8 +8,20 @@ namespace studyBuddy.programComponents.forumNeeds
 {
     class ForumPost
     {
-        /*public long id { get; private set; }
-        public string name;*/
+        public long id { get; private set; }
+        public string name;
+        public string description;
+        public int ownerId;
+
+        public ForumPost(long id, string name="", string description = "", int ownerId=0)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.ownerId = (ownerId >= 0) ? ownerId : 0;
+        }
+
+        
 
     }
 }
