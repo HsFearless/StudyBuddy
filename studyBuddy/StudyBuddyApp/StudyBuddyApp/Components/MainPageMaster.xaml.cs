d3deb8db2cd0595b81c294e46b52c4d35c5103cc
Mantas Aleksandravičius <mantas.aleksandravicius@mif.stud.vu.lt>
2019-11-02 12:47:40 +0200

    Re-done slide out bar and design

---
+++ studyBuddy/StudyBuddyApp/StudyBuddyApp/Components/MainPageMaster.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StudyBuddyApp.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public MainPageMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MainPageMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MainPageMasterMenuItem> MenuItems { get; set; }
+
+            public MainPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<MainPageMasterMenuItem>(new[]
+                {
+                    new MainPageMasterMenuItem { Id = 0, Title = "Profile" },
+                    new MainPageMasterMenuItem { Id = 1, Title = "Forums" },
+                    new MainPageMasterMenuItem { Id = 2, Title = "Find Buddy" },
+                    new MainPageMasterMenuItem { Id = 3, Title = "Help & Settings" },
+                    new MainPageMasterMenuItem { Id = 4, Title = "Sign Out" },
+                });
+            }
+
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            #endregion
+        }
+    }
+}
