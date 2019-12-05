33ad3e944083e6f005204f20dd8e9bf11eea41fd
Mantas Aleksandravičius <mantas.aleksandravicius@mif.stud.vu.lt>
2019-12-05 18:00:44 +0200

    Log in page

--- studyBuddy/StudyBuddyApp/StudyBuddyApp.Android/MainActivity.cs
+++ studyBuddy/StudyBuddyApp/StudyBuddyApp.Android/MainActivity.cs
@@ -13,7 +13,9 @@ namespace StudyBuddyApp.Droid
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         [Obsolete]
+#pragma warning disable CS0809 // Obsolete member overrides non-obsolete member
         protected override void OnCreate(Bundle savedInstanceState)
+#pragma warning restore CS0809 // Obsolete member overrides non-obsolete member
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
